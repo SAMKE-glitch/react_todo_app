0ed189c000f671d4ef71e012a4f91589a4253279
samke-glitch <sammythemwa@gmail.com>
2025-01-15 04:50:56 -0500

    toggle function and checkbox

--- todo-list-app/src/components/TodoItem.js
+++ todo-list-app/src/components/TodoItem.js
@@ -4,14 +4,18 @@ import "../styles/TodoItem.css";
 const TodoItem = ({ todo, deleteTodo, toggleComplete }) => {
   return (
     <div className="todo">
-      <div className="todo-text">{todo.text}</div>
+      <div className="todo-text">
+        <p>{todo.text}</p>
         {/* Add checkbox for toggle complete */}
         <input
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => toggleComplete(todo.id)}
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleComplete(todo.id)}
         />
-      <button className="delete-btn" onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>
+      <button className="delete-btn" onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
     </div>
   );
 };
